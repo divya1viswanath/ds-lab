929d2a2889eee506f618ad4cbef963eb9d5d7ce2
divya1viswanath <79564191+divya1viswanath@users.noreply.github.com>
2021-03-17 11:16:51 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,11 @@
+void main()
+{
+int a,b,choice;
+printf("enter first number \n");
+scanf("%d",&a);
+printf("enter second number \n");
+scanf("%d",&b);
+printf("enter your choice(1.add 2.sub 3.mutiply 4.divison)");
+scanf
+
+}
